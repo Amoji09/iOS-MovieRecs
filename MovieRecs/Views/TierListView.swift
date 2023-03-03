cee9d12e24e14df61c6068f5c3f15f4a9cf974c3
tanishqak04 <109610019+tanishqak04@users.noreply.github.com>
2023-03-02 23:25:39 -0500

    Update TierListView.swift
    - made the tier list view not occupy the whole screen
    - changed background of safe area and readjusted colors of each tier 
    - created navigation link 
    - created search view file and linked them

--- MovieRecs/Views/TierListView.swift
+++ MovieRecs/Views/TierListView.swift
@@ -1,150 +1,71 @@
-//
-//  TierListView.swift
-//  MovieTiers
-//
-//  Created by Abhinn Vegesna on 10/13/22.
-//
-
 import SwiftUI
 
 struct TierListView: View {
     //var data = ["S", "A", "B", "C", "D", "E", "F"]
-
-    
     var body: some View {
         ZStack {
-            Color.black//.ignoresSafeArea()
+            Color.black
+            .ignoresSafeArea()
             //.ignoresSafeArea()
             //.background(Color.red)
-        VStack(alignment: .leading, spacing: 4) {
-            
-            //ForEach(data,id: \.self){ data in
-            //Color.red
-            //.ignoresSafeArea()
-            //.background(Color.red)
-            //Color.red
-            TierView()
+            VStack {
+                //ForEach(data,id: \.self){ data in
+                //Color.red
+                //.ignoresSafeArea()
+                //.background(Color.red)
+                //Color.red
+                TierView(tierType: "S", color: Color(red: 237/255, green: 15/255, blue: 80/255))
+                TierView(tierType: "A", color: Color(red: 203/255, green: 48/255, blue: 96/255))
+                TierView(tierType: "B", color: Color(red: 190/255, green: 49/255, blue: 184/255))
+                TierView(tierType: "C", color: Color(red: 209/255, green: 74/255, blue: 150/255))
+                TierView(tierType: "D", color: Color(red: 223/255, green: 129/255, blue: 51/255))
+                TierView(tierType: "F", color: Color(red: 244/255, green: 214/255, blue: 47/255))
                 //.lineSpacing(20)
-            .padding(EdgeInsets(top: 3, leading: 0, bottom: 3, trailing: 0))
-
-                    
-                    
+                
             }
         }
-                
-        .background(Color(red: 28/255, green: 39/255, blue: 52/255))
-        
     }
-        
+    
+    struct TierListViewNav: View {
+       var body: some View {
+          NavigationView {
+             NavigationLink(destination: SearchView()) {
+             }.buttonStyle(PlainButtonStyle())
+          }
+       }
     }
-
-
-
-//struct TierView: View {
-  //  var body: some View{
+    //struct TierView: View {
+    //  var body: some View{
     //    ZStack {
-      //  .background(Color(red: 20/255, green: 14/255, //blue: 12/255))
+    //  .background(Color(red: 20/255, green: 14/255, //blue: 12/255))
     //}
-//}
-
-struct TierView: View {
-    var body: some View{
-        HStack{
-            //Color.red
-            Spacer(minLength: 40)
-            RoundedRectangle(cornerRadius: 12)
-                .fill(Color(red: 87/255, green: 44/255, blue: 212/255))
-                .frame(width: 120, height: 120)
-                .overlay(Text("S").font(.largeTitle))
-            Spacer(minLength: 6)
-            RoundedRectangle(cornerRadius: 5)
-                .fill(Color(red: 28/255, green: 39/255, blue: 52/255))
-                .frame(width: 330, height: 120)
-            Spacer(minLength: 0)
+    //}
+    
+    struct TierView: View {
+        let tierType: String
+        let color: Color
+        var body: some View{
+            
+            HStack{
+                //Color.red
+                Spacer(minLength: 40)
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(color)
+                    .frame(width: 120, height: 120)
+                    .overlay(Text(tierType).font(.largeTitle))
+                Spacer(minLength: 6)
+                RoundedRectangle(cornerRadius: 5)
+                    .fill(Color(red: 28/255, green: 39/255, blue: 52/255))
+                    .frame(width: 330, height: 120)
+                Spacer(minLength: 0)
             }
-        HStack{
-            Spacer(minLength: 40)
-            RoundedRectangle(cornerRadius: 5)
-                .fill(Color(red: 125/255, green: 53/255, blue: 226/255))
-                .frame(width: 120, height: 120)
-                .overlay(Text("A").font(.largeTitle))
-            Spacer(minLength: 6)
-            RoundedRectangle(cornerRadius: 5)
-                .fill(Color(red: 28/255, green: 39/255, blue: 52/255))
-                .frame(width: 330, height: 120)
-            Spacer(minLength: 0)
-            }
-        HStack{
-            Spacer(minLength: 40)
-            RoundedRectangle(cornerRadius: 5)
-                .fill(Color(red: 131/255, green: 90/255, blue: 172/255))
-                .frame(width: 120, height: 120)
-                .overlay(Text("B").font(.largeTitle))
-            Spacer(minLength: 6)
-            RoundedRectangle(cornerRadius: 5)
-                .fill(Color(red: 28/255, green: 39/255, blue: 52/255))
-                .frame(width: 330, height: 120)
-            Spacer(minLength: 0)
-            }
-        HStack{
-            Spacer(minLength: 40)
-            RoundedRectangle(cornerRadius: 5)
-                .fill(Color(red: 168/255, green: 69/255, blue: 168/255))
-                .frame(width: 120, height: 120)
-                .overlay(Text("C").font(.largeTitle))
-            Spacer(minLength: 6)
-            RoundedRectangle(cornerRadius: 5)
-                .fill(Color(red: 28/255, green: 39/255, blue: 52/255))
-                .frame(width: 330, height: 120)
-            Spacer(minLength: 0)
-            }
-        HStack{
-            Spacer(minLength: 40)
-            RoundedRectangle(cornerRadius: 5)
-                .fill(Color(red: 212/255, green: 77/255, blue: 120/255))
-                .frame(width: 120, height: 120)
-                .overlay(Text("D").font(.largeTitle))
-            Spacer(minLength: 6)
-            RoundedRectangle(cornerRadius: 5)
-                .fill(Color(red: 28/255, green: 39/255, blue: 52/255))
-                .frame(width: 330, height: 120)
-            Spacer(minLength: 0)
-            }
-        HStack{
-            Spacer(minLength: 40)
-            RoundedRectangle(cornerRadius: 5)
-                .fill(Color(red: 217/255, green: 87/255, blue: 103/255))
-                .frame(width: 120, height: 120)
-                .overlay(Text("E").font(.largeTitle))
-            Spacer(minLength: 6)
-            RoundedRectangle(cornerRadius: 5)
-                .fill(Color(red: 28/255, green: 39/255, blue: 52/255))
-                .frame(width: 330, height: 120)
-            Spacer(minLength: 0)
-            }
-        HStack{
-            Spacer(minLength: 40)
-            RoundedRectangle(cornerRadius: 5)
-                .fill(Color(red: 236/255, green: 33/255, blue: 71/255))
-                .frame(width: 120, height: 120)
-                .overlay(Text("F").font(.largeTitle))
-            Spacer(minLength: 6)
-            RoundedRectangle(cornerRadius: 5)
-                .fill(Color(red: 28/255, green: 39/255, blue: 52/255))
-                .frame(width: 330, height: 120)
-            Spacer(minLength: 0)
-            }
-        
+        }
     }
-    var oneTier: String
-    init(){
-        oneTier = "S"
-        print(oneTier);
+    
+    struct TierListView_Previews: PreviewProvider {
+        static var previews: some View {
+            TierListView()
+        }
     }
 }
 
-struct TierListView_Previews: PreviewProvider {
-    static var previews: some View {
-        TierListView()
-    }
-}
