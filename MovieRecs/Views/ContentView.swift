c2ba9cf92f242c82ad7e1c818f549343c1248336
Amogh Mantri <amogh.mantri@yahoo.com>
2023-03-07 16:05:13 -0500

    modified search view

--- MovieRecs/Views/ContentView.swift
+++ MovieRecs/Views/ContentView.swift
@@ -2,30 +2,9 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @StateObject var api = APIModel()
-    @State var query = ""
-    @State var movieTitle = "Default"
-    @State var movieResults : [TMDBMovie] = []
-    var body: some View{
-        VStack{
-            ForEach(movieResults) { movie in
-                MovieResultView(movie: movie)
-            }
-            HStack{
-                TextField("Movie Title", text: $query)
-                Button(action:{
-                    if (query.count > 0){
-                        print("Call api")
-                        api.fetchFilms(movieName: query, completionHandler: {(films) in
-                            self.movieResults = films
-                        })
-                    }
-                }){
-                    Text("Search")
-                }
-            }
-        }
-    }
+  var body: some View{
+    Text("hello")
+  }
 }
 
 struct MovieResultView : View{
