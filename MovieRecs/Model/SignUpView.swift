c1fa8d1f677e6f2c59fcb37b295d56c2f25ec81f
Miggy554 <104393035+Miggy554@users.noreply.github.com>
2023-03-07 18:22:49 -0500

    Create SignUpView.swift

---
+++ MovieRecs/Model/SignUpView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct SignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpView()
+    }
+}
